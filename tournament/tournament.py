0ae4923500ab1e6f3a23b668d0ff0fc8bb612f7e
Jason Ling <jtg2078@gmail.com>
2015-04-26 18:21:27 +0800

    added code ensure pair only never-met players

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -99,7 +99,7 @@ def reportMatch(winner, loser, c=None):
     c.execute("insert into matches (winner, loser) values (%s, %s)", (winner, loser))
 
 
-def swissPairings():
+def swissPairings_old():
     """Returns a list of pairs of players for the next round of a match.
   
     Assuming that there are an even number of players registered, each player
@@ -120,3 +120,62 @@ def swissPairings():
               standings[x+1][0],
               standings[x+1][1]) for x in xrange(0, len(standings), 2)]
     return pairs
+
+
+@connect_db
+def swissPairings(c=None):
+    """Returns a list of pairs of players for the next round of a match.
+
+    Assuming that there are an even number of players registered, each player
+    appears exactly once in the pairings.  Each player is paired with another
+    player with an equal or nearly-equal win record, that is, a player adjacent
+    to him or her in the standings.
+
+    Returns:
+      A list of tuples, each of which contains (id1, name1, id2, name2)
+        id1: the first player's unique id
+        name1: the first player's name
+        id2: the second player's unique id
+        name2: the second player's name
+    """
+
+    # we need to create a map to keep track which players already played which players
+    matches_map = dict()
+    c.execute("select winner, loser from matches")
+    for p1, p2 in c.fetchall():
+        if p1 in matches_map:
+            matches_map[p1].add(p2)
+        else:
+            matches_map[p1] = set([p2])
+        if p2 in matches_map:
+            matches_map[p2].add(p1)
+        else:
+            matches_map[p2] = set([p1])
+
+    # try to group match base on standinds
+    standings = playerStandings()
+    count = len(standings)
+    assigned = set()
+    pairs = list()
+    for x in range(count):
+        p1_id = standings[x][0]
+        p1_name = standings[x][1]
+        if p1_id in assigned:
+            continue
+        candidate_found = False
+        for y in range(x+1, count):
+            p2_id = standings[y][0]
+            p2_name = standings[y][1]
+            if p2_id in assigned:
+                continue
+            if p2_id not in matches_map[p1_id]:
+                candidate_found = True
+                pairs.append((p1_id, p1_name, p2_id, p2_name))
+                assigned.add(p2_id)
+                break
+        if not candidate_found:
+            raise ValueError("unable to find a new opponent for player {0}".format(p1_name))
+    return pairs
+
+
+
